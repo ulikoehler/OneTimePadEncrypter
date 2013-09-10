f7d520c27f0d60b36c0da911d8643035835f5380
quant42 <yann_spoeri@web.de>
2013-09-10 20:38:46 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: quant
+ *
+ * Created on September 10, 2013, 8:32 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
